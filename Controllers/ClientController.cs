5cd869677bd03a2bda6d437c0c7e95d9e38cd437
Ankit Thakkar <AnkitT@contrado.com>
2021-03-25 10:56:52 +0530

    first commit

---
+++ Controllers/ClientController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NetCoreProject.Models;
+namespace NetCoreProject.Controllers
+{
+    public class ClientController : Controller
+    {
+        private static List<ClientViewModel> Clients = new List<ClientViewModel>()
+        {
+            new ClientViewModel
+            {
+                Name = "Gonzalo",
+                Lastname = "Rodriguez",
+                Age = 22
+            },
+            new ClientViewModel
+            {
+                Name = "Hernan",
+                Lastname = "Pereira",
+                Age = 11
+            },
+            new ClientViewModel
+            {
+                Name = "Ankit",
+                Lastname = "Thakkar",
+                Age = 25
+            },
+        };
+        // GET: Client
+        public IActionResult Index()
+        {
+            return View(Clients);
+        }
+    }
+}
